facfa19cd728636d2fe98cb7d208d719338d9046
Terrorist4 <jhidalgo934@boscdelacoma.cat>
2026-05-11 12:20:31 +0200

    Update Clau.java

--- Clau.java
+++ Clau.java
@@ -1,10 +1,7 @@
 public class Clau extends Item {
     private String idPany;
 
-    public Clau(String clau1) {
-        super("calu1",  "Una Clau", true);
-        this.idPany = idPany;
-    }
+    
 
     public String getidPany() {
         return idPany;
